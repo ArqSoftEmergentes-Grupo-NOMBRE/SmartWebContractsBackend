58bd6187bc8de8c15db9824a7253c51992c46018
Cesar <u202218735@upc.edu.pe>
2025-06-02 23:36:25 -0500

    Initialize base structure of the SmartWebContracts project
    Co-authored-by: Joaquin Marcelo <u202114545@upc.edu.pe>
    Co-authored-by: Ramón Alejandro <u20221D126@upc.edu.pe>
    Co-authored-by: Fernando Josué <u202112722@upc.edu.pe>

---
+++ src/test/java/com/smart/smartwebcontracts/SmartwebcontractsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart.smartwebcontracts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartwebcontractsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
