8ce59529a5e686d02cd83511e64341b7729a4be0
Salvlormonch1 <salvlormonch@gmail.com>
2025-06-16 18:41:33 -0500

    feat(deliverable): created Deliverable entity.

---
+++ src/main/java/com/smart/smartwebcontracts/deliverables/domain/model/Deliverable.java
@@ -0,0 +1,42 @@
+package com.smart.smartwebcontracts.deliverables.domain.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+import java.util.*;
+
+@Entity
+@Table(name = "deliverables")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Deliverable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private float price;
+
+    @Column(nullable = false)
+    private String status;
+
+    // Relations
+    @Column(name = "contract_id", nullable = false)
+    private UUID contractId;
+
+    // Methods
+    public void ApproveDeliverable() { this.status = "Approved"; }
+}
